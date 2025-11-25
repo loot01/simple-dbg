f92ad3aeb7a5a45b1729a0bd9f4a745510531c59
suleif <stargaming654@gmail.com>
2025-11-25 16:42:13 +0100

    4: Added basic file parsing and a hexdump function

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,37 @@
 use std::env;
-//use std::fs;
 use std::process;
+use std::path::Path;
+use std::fs;
 
 fn main() -> process::ExitCode {
     let args: Vec<String> = env::args().collect();
     let mut flags: [bool; 10] = [false; 10];
     let mut file_name = String::new();
+    let file_buf : Vec<u8>;
+
     if args.len() < 2 {
         print_usage();
         return process::ExitCode::FAILURE;
     }
+
     check_args(&args, &mut flags, &mut file_name);
+
     if file_name.is_empty() {
         print_usage();
         return process::ExitCode::FAILURE;
     }
+
+    file_buf = match fs::read(&Path::new(&file_name)) {
+            Err(why) => panic!("Couldn't open {file_name}: {why}"),
+            Ok(file_buf) => file_buf,
+    };
+    hexdump(&file_buf);
     return process::ExitCode::SUCCESS;
 }
 
 fn check_args(args: &Vec<String>, _flags: &mut [bool; 10], file_name: &mut String) {
     let mut file_name_found: bool = false;
+
     for arg in args[1..].iter() {
         if arg.chars().nth(0).unwrap() == '-'
         {
@@ -28,6 +40,7 @@ fn check_args(args: &Vec<String>, _flags: &mut [bool; 10], file_name: &mut Strin
                 _ => println!("inexistant flag ENTERED!"),
             }
         }
+
         else {
             if !file_name_found {
                 *file_name = arg.clone();
@@ -40,4 +53,15 @@ fn check_args(args: &Vec<String>, _flags: &mut [bool; 10], file_name: &mut Strin
 fn print_usage() {
     println!("Usage: ./simple-dbg [FLAGS] [BINARY]");
     println!("FLAGS: <TODO>");
+}
+
+fn hexdump(buffer: &Vec<u8>)
+{
+    for (i, byte_pair) in buffer.chunks_exact(2).enumerate() {
+        let short = u16::from_le_bytes([byte_pair[0], byte_pair[1]]);
+        print!("{short:0>4x} ");
+        if (i + 1) % 8 == 0  {
+            print!("\n");
+        }
+    }
 }
